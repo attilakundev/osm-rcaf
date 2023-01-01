00a89a7e2a229c6a6de9d87fe18aa1e2b7577865
Attila Kun <attila@attilakundev.com>
2023-01-02 00:00:44 +0100

    Added false tests for analyzer.

--- test/integration/test_analyzer_highway_from_file_false_tests.py
+++ test/integration/test_analyzer_highway_from_file_false_tests.py
@@ -16,12 +16,60 @@ analyzer = Analyzer()
 data_parser = OSMDataParser()
 
 
-def test_route_not_split_oneway():
-    # Roundabout near Gödöllő, it used to be more complex though like what's in Zamárdi
-    file_path = f"{project_path}/test/files/results_analyzer_false/route_not_split_nooneway.xml"
+def test_route_forward_not_split_nooneway_multiple_fwd():
+    file_path = f"{project_path}/test/files/results_analyzer_false/route_forward_not_split_nooneway_multiple_fwd.xml"
     file = open(file_path, "r").read()
     data = xmltodict.parse(file)
     error_information, correct_ways_count = analyzer.relation_checking(data)
     assert len(error_information) == 1
     assert error_information[0].error_type == "Forward and non-oneway without ability to move backward"
     assert correct_ways_count == 3
+
+
+def test_route_forward_not_split_nooneway_only_one_fwd():
+    file_path = f"{project_path}/test/files/results_analyzer_false/route_forward_not_split_nooneway_only_one_fwd.xml"
+    file = open(file_path, "r").read()
+    data = xmltodict.parse(file)
+    error_information, correct_ways_count = analyzer.relation_checking(data)
+    assert len(error_information) == 1
+    assert error_information[0].error_type == "Forward and non-oneway without ability to move backward"
+    assert correct_ways_count == 2
+
+
+def test_route_simple_gap_in_road():
+    file_path = f"{project_path}/test/files/results_analyzer_false/route_simple_gap_in_road.xml"
+    file = open(file_path, "r").read()
+    data = xmltodict.parse(file)
+    error_information, correct_ways_count = analyzer.relation_checking(data)
+    assert len(error_information) == 1
+    assert error_information[0].error_type == "Gap"
+    assert correct_ways_count == 3
+
+
+def test_route_oneway_without_role():
+    file_path = f"{project_path}/test/files/results_analyzer_false/route_oneway_without_role.xml"
+    file = open(file_path, "r").read()
+    data = xmltodict.parse(file)
+    error_information, correct_ways_count = analyzer.relation_checking(data)
+    assert len(error_information) == 1
+    assert error_information[0].error_type == "Wrong role setup"
+    assert correct_ways_count == 2
+
+
+def test_route_forward_split_oneway_gap():
+    file_path = f"{project_path}/test/files/results_analyzer_false/route_forward_split_oneway_gap.xml"
+    file = open(file_path, "r").read()
+    data = xmltodict.parse(file)
+    error_information, correct_ways_count = analyzer.relation_checking(data)
+    assert len(error_information) == 1
+    assert error_information[0].error_type == "Gap in forward series"
+    assert correct_ways_count == 5
+
+def test_route_norole_split_nooneway():
+    file_path = f"{project_path}/test/files/results_analyzer_false/route_norole_split_nooneway.xml"
+    file = open(file_path, "r").read()
+    data = xmltodict.parse(file)
+    error_information, correct_ways_count = analyzer.relation_checking(data)
+    assert len(error_information) == 1
+    assert error_information[0].error_type == "Gap"
+    assert correct_ways_count == 5
